00f9b83fb95027a77c9f68eab142ed0e71dee62c
Stephen Hamilton <stephenhamilton@mac.com>
2019-05-05 01:14:04 +0100

    disables broken pageNotFound - ready for express
    Signed-off-by: Stephen Hamilton <stephenhamilton@mac.com>

--- www/src/js/routes/mainRoutes.js
+++ www/src/js/routes/mainRoutes.js
@@ -22,9 +22,9 @@ const Routes = (
       <Redirect exact from="/" to="/about" />
       <Route path="/:category_id/:client_id/:entry_id" component={Item} />
       <Route path="/:category_id/:year_id" component={Categories} />
-      <Route component={PageNotFound} />
+      {/*<Route component={PageNotFound} />*/}
       {/*<Route component={() => (window.location = '404.php')} />*/}
-      {/*<Redirect to="/about" />*/}
+      <Redirect to="/about" />
     </Switch>
     <Footer />
   </React.Fragment>
