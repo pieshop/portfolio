8bf4bff0f643a9c34c2f2e8bbcb865ef0d465c10
Stephen Hamilton <stephenhamilton@mac.com>
2026-02-20 12:52:16 +0000

    ui amends
    Signed-off-by: Stephen Hamilton <stephenhamilton@mac.com>

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router';
-import { Flex } from '@radix-ui/themes';
+import { Container, Flex } from '@radix-ui/themes';
 import Routes from './routes/mainRoutes';
 import Loader from './containers/Loader';
 
@@ -8,11 +8,13 @@ const App: React.FC = () => {
   return (
     <React.Fragment>
       <Loader />
-      <Flex direction="column" minHeight="100vh" px={{ initial: '3', md: '5' }}>
-        <BrowserRouter>
-          <Routes />
-        </BrowserRouter>
-      </Flex>
+      <Container size="4" align="center">
+        <Flex direction="column" minHeight="100vh" px={{ initial: '3', md: '5' }}>
+          <BrowserRouter>
+            <Routes />
+          </BrowserRouter>
+        </Flex>
+      </Container>
     </React.Fragment>
   );
 };
