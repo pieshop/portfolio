65b9fca2bd57d4e31cde07faeda200d07f3a6b76
Stephen Hamilton <stephenhamilton@mac.com>
2026-02-21 16:04:47 +0000

    delete refs
    Signed-off-by: Stephen Hamilton <stephenhamilton@mac.com>

--- www-orig/buildtools/webpack_config/constants/paths-mini.js
+++
@@ -1,8 +0,0 @@
-const base = require(`./paths`);
-const outputDirectory = base.dist.replace('{dir}', 'dev');
-const specific = {
-  dist : outputDirectory,
-  htmlOutPath : outputDirectory,
-};
-const merged = {...base, ...specific};
-module.exports = merged;
